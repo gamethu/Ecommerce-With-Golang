7caa3020e6be0313ebbc35ee9879b61664b2cdae
NguyenTanDai_3122411035 <70790046+gamethu@users.noreply.github.com>
2026-05-19 18:13:15 +0700

    Implement user signup functionality with validation and error handling

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,17 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"Repositories/E-COM/models"
+
+)
 
 func HashPassword(password string) string {
 
@@ -11,7 +22,48 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 }
 
 func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				gin.H{"error": err.Error()},
+			)
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already existed"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"error": ""})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone number is already existed"})
+			return
+		}
+	}
 }
 
 func Login() gin.HandlerFunc {
